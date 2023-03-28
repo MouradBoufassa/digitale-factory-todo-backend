f2fd355a4fb221a881b5669eb76c94636438f4c0
Khaled <attano001@outlook.com>
2023-03-28 12:35:34 +0100

    initialize node.js project with yarn

---
+++ src/app.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-unused-vars */
+// * Imports
+const express = require('express');
+const cors = require('cors');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+const morgan = require('morgan');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// * Middleware 
+app.use(cors({ origin: process.env.CLIENT_URL }));
+app.use(express.urlencoded({ extended: false, limit: '1mb' }));
+if (process.env.NODE_ENV === 'DEVELOPMENT') app.use(morgan('tiny'));
+
+// * Routes
+
+// * Connection
+app.listen(PORT, () => {
+    console.log('---------------------');
+    console.log(
+        'Dev server running at  >'.green,
+        `http://localhost:${PORT}`.blue
+    );
+});
