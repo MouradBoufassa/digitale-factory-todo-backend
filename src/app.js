a223ec365295f3519b8d2f8ea2059ed3020c2e75
Khaled <attano001@outlook.com>
2023-03-31 20:14:05 +0100

    refactor: remove a duplicate line

--- src/app.js
+++ src/app.js
@@ -14,7 +14,6 @@ const PORT = process.env.PORT || 5000;
 const connectDB = require('./config/connectDB');
 const dueDateJob = require('./config/cron');
 const swaggerSpecs = require('./config/swaggerOptions');
-const swaggerSpecs = require('./config/swaggerOptions');
 
 // * Middleware 
 app.use(cors({ origin: process.env.CLIENT_URL }));
